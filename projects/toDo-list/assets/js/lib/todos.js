d021d9c31c9fc506f444ac55f89c9fe28ee0c107
pplou <lousilva@gmail.com>
2017-11-08 23:41:06 +0000

    To-do List

--- projects/toDo-list/assets/js/lib/todos.js
+++ projects/toDo-list/assets/js/lib/todos.js
@@ -1,11 +1,23 @@
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
     //console.log("li clicado");
     $(this).toggleClass("completed");
 });
 
-$("span").on("click", function(event) {
-    $(this).parent().fadeOut(500, function() {
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(350, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash aria-hidden='true'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus-circle").click(function() {
+    $("input[type='text']").fadeToggle();
 });
