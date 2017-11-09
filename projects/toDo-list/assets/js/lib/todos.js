e8fac5fb086e6be2d2d455447d6329e09cfe092e
pplou <lousilva@gmail.com>
2017-11-09 22:32:31 +0000

    Todo List updated

--- projects/toDo-list/assets/js/lib/todos.js
+++ projects/toDo-list/assets/js/lib/todos.js
@@ -1,11 +1,12 @@
 $("ul").on("click", "li", function() {
-    //console.log("li clicado");
     $(this).toggleClass("completed");
+    saveState();
 });
 
 $("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(350, function() {
         $(this).remove();
+        saveState();
     });
     event.stopPropagation();
 });
@@ -15,9 +16,31 @@ $("input[type='text']").keypress(function(event) {
         var todoText = $(this).val();
         $(this).val("");
         $("ul").append("<li><span><i class='fa fa-trash aria-hidden='true'></i></span> " + todoText + "</li>");
+        saveState();
     }
 });
+
+// load whatever is in the local storage
+if (localStorage.getItem('todos')) {
+    $('#todos').html(localStorage.getItem('todos'));
+}
+
+/*
+// deletes all contents of the local storage
+$('#clear').click(function() {
+    window.localStorage.clear();
+    location.reload();
+    return false;
+});
+*/
 
 $(".fa-plus-circle").click(function() {
     $("input[type='text']").fadeToggle();
 });
+
+//Saves current html from the ul with id="todos"
+function saveState() {
+    var todos = $("#todos").html();
+    localStorage.setItem("todos", todos);
+    return false;
+}
